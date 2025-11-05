14479a65865c9489da39d408d711d526b41eda56
bwmx <blairmcilroy@icloud.com>
2025-11-05 14:26:31 +0700

    test: fix RandomnessBeacon contract tests with vrfVerify mock, more tests to rule out other issues with error.

--- projects/randomness-beacon/smart_contracts/randomness_beacon/contract.algo.spec.ts
+++ projects/randomness-beacon/smart_contracts/randomness_beacon/contract.algo.spec.ts
@@ -1,11 +1,36 @@
-import { arc4, assert, assertMatch, emit, Global, gtxn, op, uint64 } from '@algorandfoundation/algorand-typescript'
+import {
+  arc4,
+  assert,
+  assertMatch,
+  Bytes,
+  bytes,
+  emit,
+  Global,
+  gtxn,
+  op,
+  uint64,
+  VrfVerify,
+} from '@algorandfoundation/algorand-typescript'
 import { ApplicationSpy, TestExecutionContext } from '@algorandfoundation/algorand-typescript-testing'
-import { afterEach, beforeAll, describe, expect, it } from 'vitest'
+import { afterEach, beforeAll, describe, expect, it, Mock, vi } from 'vitest'
 import { RandomnessBeacon } from './contract.algo'
 import { ExampleCaller } from './contracts/example-caller.algo'
 
 import libvrf from '../../../libvrf'
-import { RandomnessRequest, RequestCreated, VrfProof, VrfPublicKey } from './types.algo'
+import { RandomnessRequest, RequestCreated, VrfOutput, VrfProof, VrfPublicKey } from './types.algo'
+
+// Mock the op module from algorand-typescript, not the testing library
+vi.mock(import('@algorandfoundation/algorand-typescript-testing/internal'), async (importOriginal) => {
+  const mod: any = await importOriginal()
+
+  return {
+    ...mod,
+    op: {
+      ...mod.op,
+      vrfVerify: vi.fn(),
+    },
+  }
+})
 
 describe('RandomnessBeacon contract', () => {
   const ctx = new TestExecutionContext()
@@ -21,6 +46,11 @@ describe('RandomnessBeacon contract', () => {
   })
 
   const deploy = (maxPendingRequests: uint64, maxFutureRound: uint64, staleRequestTimeout: uint64) => {
+    // make creator account
+    const creatorAccount = ctx.any.account()
+    // set default sender
+    ctx.defaultSender = creatorAccount
+
     const beaconContract = ctx.contract.create(RandomnessBeacon)
 
     const beaconApp = ctx.ledger.getApplicationForContract(beaconContract)
@@ -29,7 +59,7 @@ describe('RandomnessBeacon contract', () => {
     const { publicKey, secretKey } = libvrf.keypair()
 
     beaconContract.createApplication(
-      publicKey.buffer as any as VrfPublicKey,
+      publicKey as any as VrfPublicKey,
       maxPendingRequests, // max pending requests
       maxFutureRound, // max future round
       staleRequestTimeout, // stale request timeout
@@ -70,7 +100,7 @@ describe('RandomnessBeacon contract', () => {
       const feesPaid: uint64 = costsPayment.amount - boxMbr
 
       // get next available request id
-      const requestId = beaconContract.nextRequestId.value
+      const requestId: uint64 = beaconContract.nextRequestId.value
       // inc current requestId
       beaconContract.nextRequestId.value += 1
 
@@ -93,7 +123,7 @@ describe('RandomnessBeacon contract', () => {
       //  emit created event
       emit<RequestCreated>({
         requestId: requestId,
-        requesterAppId: Global.callerApplicationId,
+        requesterAppId: exampleCallerAppId,
         requesterAddress: requesterAddress,
         round: round,
       })
@@ -102,35 +132,36 @@ describe('RandomnessBeacon contract', () => {
     })
 
     //TODO: fix completeRequest spy see below commented test also
-    spy.on.completeRequest((itxnContext) => {
-      console.log('hi')
+    // spy.on.completeRequest((itxnContext) => {
+    //   console.log('hi')
 
-      // workaround lack of vrfVerify in testing
-      // just use libvrf instead
-      const requestId: uint64 = arc4.decodeArc4(itxnContext.appArgs(1))
-      const proof: VrfProof = arc4.decodeArc4(itxnContext.appArgs(2))
+    //   // workaround lack of vrfVerify in testing
+    //   // just use libvrf instead
+    //   const requestId: uint64 = arc4.decodeArc4(itxnContext.appArgs(1))
+    //   const proof: VrfProof = arc4.decodeArc4(itxnContext.appArgs(2))
 
-      const request = beaconContract.requests(requestId).value
+    //   const request = beaconContract.requests(requestId).value
 
-      const blockSeed = op.Block.blkSeed(request.round)
+    //   const blockSeed = op.Block.blkSeed(request.round)
 
-      const { output, result } = libvrf.verify(beaconContract.publicKey, proof, blockSeed)
+    //   const { output, result } = libvrf.verify(beaconContract.publicKey, proof, blockSeed)
 
-      // TODO: call fulfillRandomness on the requester app
-      // ommited due to ApplicationSpy not working as expected in tests
-    })
+    //   // TODO: call fulfillRandomness on the requester app
+    //   // ommited due to ApplicationSpy not working as expected in tests
+    // })
 
     // add spy to test context
     ctx.addApplicationSpy(spy)
 
-    return { beaconContract, beaconApp, publicKey, secretKey }
+    return { beaconContract, beaconApp, publicKey, secretKey, manager: creatorAccount }
   }
 
   it('Can be created and global state is as expected', () => {
-    const { beaconContract, publicKey } = deploy(10, 100, 1000)
+    const { beaconContract, publicKey, manager } = deploy(10, 100, 1000)
 
     // check global state is as expected
-    expect(beaconContract.publicKey.value).toStrictEqual(publicKey.buffer)
+    expect(beaconContract.manager().native).toStrictEqual(manager)
+    expect(beaconContract.publicKey.value).toStrictEqual(publicKey)
     expect(beaconContract.nextRequestId.value).toStrictEqual(1)
     expect(beaconContract.totalPendingRequests.value).toStrictEqual(0)
     expect(beaconContract.maxPendingRequests.value).toStrictEqual(10)
@@ -147,19 +178,22 @@ describe('RandomnessBeacon contract', () => {
     // get handle to app on ledger
     const exampleCallerApp = ctx.ledger.getApplicationForContract(exampleCallerContract)
     // generate new a new account to represent the requester
-    const requesterAddress = ctx.any.account()
+    const requesterAccount = ctx.any.account()
 
     const { fees, boxMbr } = beaconContract.getCosts()
 
     // send to the caller app (this will pay beacon on our behalf)
     const costPayment = ctx.any.txn.payment({
-      sender: requesterAddress,
+      sender: requesterAccount,
       receiver: exampleCallerApp.address,
       amount: fees + boxMbr,
     })
 
     // set for test so ApplicationSpy hooks know
     exampleCallerAppId = exampleCallerApp.id
+
+    // set default sender to requester
+    ctx.defaultSender = requesterAccount
 
     // call test1 method, get a requestid and target round in return
     const [requestId, targetRound] = exampleCallerContract.test1(costPayment)
@@ -176,7 +210,7 @@ describe('RandomnessBeacon contract', () => {
     // check everything stored correctly
     expect(BigInt(storedRequest.createdAt)).toBeLessThan(BigInt(Global.round))
     expect(storedRequest.requesterAppId).toEqual(exampleCallerApp.id)
-    expect(storedRequest.requesterAddress.native).toEqual(requesterAddress)
+    expect(storedRequest.requesterAddress.native).toEqual(requesterAccount)
     expect(storedRequest.round).toEqual(targetRound)
     expect(storedRequest.costs.fees).toEqual(fees)
     expect(storedRequest.costs.boxMbr).toEqual(boxMbr)
@@ -184,7 +218,8 @@ describe('RandomnessBeacon contract', () => {
 
   // TODO: fix test, ApplicationSpy not working as expected, the hook never gets called
   it('Can call completeRequest()', () => {
-    const { beaconContract, beaconApp, secretKey } = deploy(10, 100, 1000)
+    const { beaconContract, beaconApp, secretKey, manager } = deploy(10, 100, 1000)
+
     // create example caller contract
     const exampleCallerContract = ctx.contract.create(ExampleCaller)
     // create application (pass existing beacon app)
@@ -193,25 +228,78 @@ describe('RandomnessBeacon contract', () => {
     const exampleCallerApp = ctx.ledger.getApplicationForContract(exampleCallerContract)
     // get costs
     const { fees, boxMbr } = beaconContract.getCosts()
+
+    // make new account to represent requester
+    const requesterAccount = ctx.any.account()
+
     // build fee payment
     const feePayment = ctx.any.txn.payment({
+      sender: requesterAccount,
       receiver: exampleCallerApp.address,
       amount: fees + boxMbr,
     })
+
+    // set for test so ApplicationSpy hooks know
+    exampleCallerAppId = exampleCallerApp.id
+
+    console.log('exampleCallerApp.address:', exampleCallerApp.address)
+
+    // set default sender to requester
+    ctx.defaultSender = requesterAccount
+
     // call test1 method, returns requestId and targetRound Tuple
     const [requestId, round] = exampleCallerContract.test1(feePayment)
 
-    console.log(`test1() -> [requestId: ${requestId}, round: ${round}]`)
+    // check request created correctly
+    const createdRequest = beaconContract.requests(requestId).value
 
-    // patch target round with some dummy (empty seed data) [predictable]
-    ctx.ledger.patchBlockData(round, {
-      seed: op.bzero(32),
-    })
+    expect(BigInt(createdRequest.createdAt)).toBeLessThan(BigInt(Global.round))
+    expect(createdRequest.requesterAppId).toStrictEqual(exampleCallerApp.id)
+    expect(createdRequest.requesterAddress.native).toStrictEqual(requesterAccount)
+    expect(createdRequest.round).toStrictEqual(round)
+    expect(createdRequest.costs.fees).toStrictEqual(fees)
+    expect(createdRequest.costs.boxMbr).toStrictEqual(boxMbr)
+
+    console.log(`ExampleCaller.test1() result =  [requestId: ${requestId}, round: ${round}]`)
 
     // set round to the target round (so seed is available)
     ctx.ledger.patchGlobalData({
       round: round,
     })
+    // patch target round with some dummy (empty seed data) [predictable]
+    ctx.ledger.patchBlockData(round, {
+      seed: Bytes('abcdefghijklmnopqrstuvwxyzabcdef') as bytes<32>,
+    })
+
+    const mockedVrfVerify = op.vrfVerify as Mock<typeof op.vrfVerify>
+
+    mockedVrfVerify.mockImplementation(
+      (
+        s: VrfVerify,
+        message: bytes,
+        proof: bytes | bytes<80>,
+        publicKey: bytes | bytes<32>,
+      ): readonly [bytes<64>, boolean] => {
+        //console.log(message)
+        assert(s === VrfVerify.VrfAlgorand, 'unexpected vrf type in mock')
+
+        console.log('vrfVerify mock implementation called')
+
+        // console.log('message =', message)
+        // console.log('proof =', proof)
+        // console.log('publicKey =', publicKey)
+
+        // const { output, result } = libvrf.verify(
+        //   bytesToUint8Array(publicKey),
+        //   bytesToUint8Array(proof),
+        //   bytesToUint8Array(message),
+        // )
+        // // console.log(`output = ${output} result = ${result}`)
+
+        // return [Bytes(output) as any as VrfOutput, Boolean(result === 0)]
+        return [Bytes(new Uint8Array(64).fill(7)) as any as VrfOutput, true]
+      },
+    )
 
     // get block seed
     const blockSeed = op.Block.blkSeed(round)
@@ -220,7 +308,14 @@ describe('RandomnessBeacon contract', () => {
     // result 0 === success, must be proven succesfully
     expect(result).toEqual(0)
 
-    // todo: mock completeRequest, verify the proof in there and proceed... vrfVerify unavailable in testing
-    beaconContract.completeRequest(requestId, proof.buffer as any as VrfProof)
+    // dummy call to test mock
+    op.vrfVerify(VrfVerify.VrfAlgorand, blockSeed, Bytes(proof), beaconContract.publicKey.value)
+    // expect mock to have been called (mock function always return true, and 64 bytes filled with 7)
+    expect(mockedVrfVerify).toHaveReturnedWith([Bytes(new Uint8Array(64).fill(7)) as any as VrfOutput, true])
+
+    // change default sender to manager to call completeRequest (onlyManager() is called)
+    ctx.defaultSender = manager
+    // todo: investigate error
+    beaconContract.completeRequest(requestId, Bytes(proof) as any as VrfProof)
   })
 })
